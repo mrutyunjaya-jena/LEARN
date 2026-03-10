0f652ea6540ea8cb5c816302f9615e80b8efb0c5
Mrutyunjaya Jena <mjxtor@gmail.com>
2026-03-09 22:56:39 -0700

    python libraries added

---
+++ PYTHON/Lecture12_Libraries/matplotlib/plot_graph.py
@@ -0,0 +1,6 @@
+## matplotlib is a plotting library for the Python programming language and its numerical mathematics extension NumPy. It provides an object-oriented API for embedding plots into applications using general-purpose GUI toolkits like Tkinter, wxPython, Qt, or GTK. There is also a procedural "pylab" interface based on a state machine (like OpenGL), designed to closely resemble that of MATLAB, though its use is discouraged. SciPy makes use of Matplotlib.
+## To use matplotlib, we need to import it first
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.show()
