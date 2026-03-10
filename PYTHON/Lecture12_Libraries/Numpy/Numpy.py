724ca19711b068d29e2ccd5672c1f75ee5b220fe
Mrutyunjaya Jena <mjxtor@gmail.com>
2026-03-09 21:55:37 -0700

    python libraries added

---
+++ PYTHON/Lecture12_Libraries/Numpy/Numpy.py
@@ -0,0 +1,10 @@
+## Library Numpy
+## to use numpy, we need to import it first
+import numpy as np
+
+## Numpy is a powerful library for numerical computing in Python. It provides support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays efficiently.
+
+## Creating Numpy Arrays
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
