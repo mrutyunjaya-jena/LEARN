e7db67b73dd6de260ce12db849083f073ad0cef3
Mrutyunjaya Jena <mjxtor@gmail.com>
2026-03-15 03:09:52 -0700

    STL more learning added

--- CPP/STL/1.CONTAINERS/B.Associative_Containers/MAP/map.cpp
+++ CPP/STL/1.CONTAINERS/B.Associative_Containers/MAP/map.cpp
@@ -10,7 +10,24 @@ int main(){
 
     map1 = {{1, "sun"}, {2, "mon"}, {3, "tue"}, {4, "wed"}, {5, "thu"}, {6, "fri"}, {7, "sat"}};
 
-    map1[8] = "holiday";
+    map1[8] = "holiday"; // inserting method 1
+    map1.insert({9, "workday"}); // inserting method 2
+    //more methods to insert: emplace, emplace_hint, insert_or_assign
+    map1.emplace(10, "weekend"); // inserting method 3
+    map1.emplace_hint(map1.end(), 11, "holiday"); // inserting method 4
+    map1.insert_or_assign(12, "vacation"); // inserting method 5
+
+    //another methods to delete: erase, clear
+    map1.erase(12); // deleting method 1
+    // map1.clear(); // deleting method 2
+
+    // accessing method 1
+    cout << "Accessing method 1: " << map1[1] << endl;
+    // accessing method 2
+    cout << "Accessing method 2: " << map1.at(2) << endl;
+    // accessing method 3
+    cout << "Accessing method 3: " << map1.find(3)->second << endl; 
+    
 
     for(const auto& pair : map1){
         cout << pair.first << " : " << pair.second << endl;
